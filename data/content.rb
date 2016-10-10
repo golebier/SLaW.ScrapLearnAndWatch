c65110686de5811081fb3ec448de540e22e9bdb2
Gra <golebier@gmail.com>
2016-10-10 09:15:17 +0200

    Data types, first attempt.

--- data/content.rb
+++ data/content.rb
@@ -14,34 +14,4 @@ class Content
 
   Content.property(:created_at, DateTime)
   Content.property(:updated_at, DateTime)
-  
-  
-#  Company.property(:id, Serial)
-#  Company.property(:name, Text, :required => false)
-#  Company.property(:path, Text, :required => false)
-#  Company.property(:se, Text, :required => false)
-#  Company.property(:fileformat, Text, :required => false)
-#  Company.property(:watched_counter, Text, :required => false)
-#  Company.property(:got_at, DateTime, :required => false)
-#
-#  Company.property(:watched, Boolean, :default  => false)
-#  Company.property(:moved, Boolean, :default  => false)
-#  Company.property(:removed, Boolean, :default  => false) # do not get that again
-#
-#  Company.property(:proposed, Boolean, :default  => false)
-#  Company.property(:proposed_name, Text)
-#  Company.property(:proposed_usless, Text)
-#  Company.property(:proposed_s, Integer)
-#  Company.property(:proposed_e, Integer)
-#
-#  Company.property(:fixed, Boolean, :default  => false)
-#  Company.property(:fixed_name, Text)
-#  Company.property(:fixed_usless, Text)
-#  Company.property(:fixed_s, Integer)
-#  Company.property(:fixed_e, Integer)
-#  
-#  Company.property(:from_name, Text) # url, or othen on the web side
-#  
-#  Company.property(:created_at, DateTime)
-#  Company.property(:updated_at, DateTime)
 end
--- data/contentSources.rb
+++ data/contentSources.rb
@@ -1,46 +1,15 @@
 # encoding: UTF-8
 
-class ContentSources <  Content
+class ContentSources
   include DataMapper::Resource
 
   ContentSources.property(:id, Serial)
+  ContentSources.property(:name, Text, :required => false)
+  ContentSources.property(:s, Text, :required => false)
+  ContentSources.property(:e, Text, :required => false)
   ContentSources.property(:fullOrgName, Text, :required => false)
-
-#  ContentSources.property(:id, Serial)
-#  ContentSources.property(:sourceURL, Text, :required => false)
-#  ContentSources.property(:contentURL, Text, :required => false)
-#  ContentSources.property(:used, Boolean, :default  => false)
-#
-#  ContentSources.property(:created_at, DateTime)
-#  ContentSources.property(:updated_at, DateTime)
-  
-  
-#  Company.property(:id, Serial)
-#  Company.property(:name, Text, :required => false)
-#  Company.property(:path, Text, :required => false)
-#  Company.property(:se, Text, :required => false)
-#  Company.property(:fileformat, Text, :required => false)
-#  Company.property(:watched_counter, Text, :required => false)
-#  Company.property(:got_at, DateTime, :required => false)
-#
-#  Company.property(:watched, Boolean, :default  => false)
-#  Company.property(:moved, Boolean, :default  => false)
-#  Company.property(:removed, Boolean, :default  => false) # do not get that again
-#
-#  Company.property(:proposed, Boolean, :default  => false)
-#  Company.property(:proposed_name, Text)
-#  Company.property(:proposed_usless, Text)
-#  Company.property(:proposed_s, Integer)
-#  Company.property(:proposed_e, Integer)
-#
-#  Company.property(:fixed, Boolean, :default  => false)
-#  Company.property(:fixed_name, Text)
-#  Company.property(:fixed_usless, Text)
-#  Company.property(:fixed_s, Integer)
-#  Company.property(:fixed_e, Integer)
-#  
-#  Company.property(:from_name, Text) # url, or othen on the web side
-#  
-#  Company.property(:created_at, DateTime)
-#  Company.property(:updated_at, DateTime)
+  ContentSources.property(:sourceURL, Text, :required => false)
+  ContentSources.property(:contentURL, Text, :required => false)
+  ContentSources.property(:used, Boolean, :default  => false)
+  ContentSources.property(:fullOrgName, Text, :required => false)
 end
